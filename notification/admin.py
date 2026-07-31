bb725c3b93f25ef55f0d781f35e0c12253c47dc0
Kacper <kogutkacper7@gmail.com>
2026-07-31 15:08:44 +0200

    feat: register notfication in admin.py

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from notification.models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ["id", "recipient", "content", "created_at"]
+    search_fields = ["recipient", "content"]
+    list_filter = ["created_at"]
